bb3e37718c2fead66b6e4f2e3803bf9bb8306e25
vietpham2k3 <vietvuavanvat13579@gmail.com>
2024-05-10 16:54:27 +0700

    up

--- js/detail.js
+++ js/detail.js
@@ -12,23 +12,6 @@
 
 // Thêm sự kiện scroll chỉ đối với các phần tử mà bạn muốn xử lý
 window.addEventListener('scroll', handleScroll);
-
-function spinner() {
-    //  SPINNER
-    $("#spinner").spinner();
-
-    //  INPUT ONLY NUMBERS
-    $('#spinner').keyup(function () {
-        this.value = this.value.replace(/[^1-9]/g, '');
-    });
-}
-// INPUT NUMBER MAX LENGHT
-function maxLengthCheck(object) {
-    if (object.value.length > object.maxLength)
-        object.value = object.value.slice(0, object.maxLength)
-}
-window.onload = spinner;
-
 // 
 const starsEl = document.querySelectorAll(".fa-star");
 const emojisEl = document.querySelectorAll(".far");
